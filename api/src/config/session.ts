2f1320bd1e6fd37e5758b12a4ac740d0bcc5447b
mani3354488 <mani3354488@gmail.com>
2020-10-22 17:59:31 +0530

    setting up and connecting to redis and mongoose

---
+++ api/src/config/session.ts
@@ -0,0 +1,22 @@
+import { SessionOptions } from 'express-session'
+import { IN_PROD } from './app'
+
+const HALF_HOUR = 1000 * 60 * 30 
+const {
+    SESSION_SECRET = `please keep this secret, mate`,
+    SESSION_NAME = `sid`,
+    SESSION_IDEL_TIMEOUT = HALF_HOUR,
+} = process.env
+
+export const SESSION_OPTIONS: SessionOptions = {
+    secret: SESSION_SECRET,
+    name: SESSION_NAME,
+    cookie: {
+      maxAge: +SESSION_IDEL_TIMEOUT, //+ symbol to cast it an integer
+      secure: IN_PROD,
+      sameSite: true,
+    },
+    rolling: true,
+    resave: false,
+    saveUninitialized: false,    
+}
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,26 @@
 import express from 'express'
+import mangoose, { Mongoose } from 'mongoose'
+import session from 'express-session'
+import connectRedis from 'connect-redis'
+import Redis from 'ioredis'
+import { MONGO_URI, MONGO_OPTIONS, REDIS_OPTIONS, SESSION_OPTIONS, APP_PORT } from './config'
 
-const app = express()
+;(async () => {
+  await mongoose.connect(MONGO_URI, MONGO_OPTIONS)
+  const RedisStore = connectRedis(session)
+  const client = new Redis(REDIS_OPTIONS)
 
-app.get('/', (req, res) => res.json({ message: 'OK'}))
+  const app = express()
 
-app.listen(3000, () => console.log(`http://localhost:${3000}`))
+  app.use(
+    session({
+      ...SESSION_OPTIONS, 
+      store:new RedisStore({ client })
+    })
+  )
+
+
+  app.get('/', (req, res) => res.json({ message: 'OK'}))
+
+  app.listen(APP_PORT, () => console.log(`http://localhost:${APP_PORT}`))
+})()
